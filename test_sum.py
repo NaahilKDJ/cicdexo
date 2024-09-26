6d084eb53625adcf1d8d61bf501a6a69612df65d
NaahilKDJ <134393859+NaahilKDJ@users.noreply.github.com>
2024-09-26 10:47:34 +0200

    update test_sum

--- test_sum.py
+++ test_sum.py
@@ -1,11 +1,13 @@
 import sum
 
+a = 4
+b = 5
 
-a = int(input("Entrez un nombre : \n"))
-b = int(input("Entrez un deuxième nombre : \n"))
+# a = int(input("Entrez un nombre : \n"))
+# b = int(input("Entrez un deuxième nombre : \n"))
 
 total = sum.sumNumbers(a, b)
 
-print('\n')
+# print('\n')
 
 print(f"la somme de {a} et {b} vaut {total}")
